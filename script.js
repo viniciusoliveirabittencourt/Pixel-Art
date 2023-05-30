d21592344ea9fbcfae3c83913b2f783319cc8563
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2023-05-30 09:25:57 -0300

    add script for 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const grindPixel = document.getElementById('grind-pixel');
 let selected = document.getElementsByClassName('selected')[0];
 const colors = document.getElementsByClassName('paint-collor');
+const clearPixels = document.getElementById('clear-pixels').addEventListener('click', clearAllPixels);
 
 function changeSelected(e) {
   if (e.target.id !== selected) {
@@ -11,12 +12,19 @@ function changeSelected(e) {
 }
 
 function changePixel(e) {
-  e.target.style.backgroundColor = `${getComputedStyle(selected).backgroundColor}`
-}
+  e.target.style.backgroundColor = `${getComputedStyle(selected).backgroundColor}`;
+};
+
+function clearAllPixels(_) {
+  const allPixels = document.getElementsByTagName('td');
+  for (let i = 0; i < allPixels.length; i += 1) {
+    allPixels[i].style.backgroundColor = 'white';
+  }
+};
 
 for(let i = 0; i < colors.length; i += 1) {
   colors[i].addEventListener('click', changeSelected);
-}
+};
 
 
 for(let i = 0; i < 5; i += 1) {
